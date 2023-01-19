5a43cdf57407251ce4ccc08cfbf312678e7b6957
c0admin <mustafagldren@gmail.com>
2023-01-20 01:24:53 +0300

    Proje dosyası ekle.

---
+++ ex2/ex2.cpp
@@ -0,0 +1,16 @@
+﻿// 1den 10a kadar sayilarin karelerini yazdırma
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    for (i=1 ; i <= 10; i++ ) {
+        cout << i*i << endl;
+    }
+
+    system("pause");
+    return 0;
+}
+
